7b799e9a7c4bec24427a2ae577680cce12085af0
EvilMoOd <962475158@qq.com>
2022-08-23 14:26:42 +0800

    主页修改

--- script.js
+++ script.js
@@ -1,26 +1,72 @@
-$(function () {
-    $('.sun').click(function (e) {
-        e.stopPropagation();
-        $("html").toggleClass('container-night');
-        $(".sky-night").toggleClass('sky-night-fade');
-        $("p, h1").toggleClass('text-color');
-        $(".ocean-night").toggleClass('ocean-night-fade');
-        $(".moon").toggleClass('moon-fade');
-        $(".ocean").toggleClass('animation-stop');
-        $(".bird").toggleClass('birds-fly');
-        $(".boat").toggleClass('boat-sail');
-        $(".mountain-top, .mountain-top >*").toggleClass('mountain-top-night');
-        $(".mountain-middle, .mountain-middle >*").toggleClass('mountain-middle-night');
-        $(".mountain-back, .mountain-back >*").toggleClass('mountain-back-night');
-        $(".cloud").delay(2500).fadeToggle(2500);
-        $(".stars").toggleClass('stars-fade');
-        $(".shooting-star").toggleClass('shooting');
 
-        var angle = ($('.sun-container').data('angle') + 360) || 360;
-        $('.sun-container').css({
-            'transform': 'rotate(' + angle + 'deg)'
-        });
-        $('.sun-container').data('angle', angle);
 
+/* Please ❤ this if you like it! */
+
+
+(function($) { "use strict";
+		
+	//Page cursors
+
+    document.getElementsByTagName("body")[0].addEventListener("mousemove", function(n) {
+        t.style.left = n.clientX + "px", 
+		t.style.top = n.clientY + "px", 
+		e.style.left = n.clientX + "px", 
+		e.style.top = n.clientY + "px", 
+		i.style.left = n.clientX + "px", 
+		i.style.top = n.clientY + "px"
     });
-});
+    var t = document.getElementById("cursor"),
+        e = document.getElementById("cursor2"),
+        i = document.getElementById("cursor3");
+    function n(t) {
+        e.classList.add("hover"), i.classList.add("hover")
+    }
+    function s(t) {
+        e.classList.remove("hover"), i.classList.remove("hover")
+    }
+    s();
+    for (var r = document.querySelectorAll(".hover-target"), a = r.length - 1; a >= 0; a--) {
+        o(r[a])
+    }
+    function o(t) {
+        t.addEventListener("mouseover", n), t.addEventListener("mouseout", s)
+    }
+	
+	//Navigation
+
+	var app = function () {
+		var body = undefined;
+		var menu = undefined;
+		var menuItems = undefined;
+		var init = function init() {
+			body = document.querySelector('body');
+			menu = document.querySelector('.menu-icon');
+			menuItems = document.querySelectorAll('.nav__list-item');
+			applyListeners();
+		};
+		var applyListeners = function applyListeners() {
+			menu.addEventListener('click', function () {
+				return toggleClass(body, 'nav-active');
+			});
+		};
+		var toggleClass = function toggleClass(element, stringClass) {
+			if (element.classList.contains(stringClass)) element.classList.remove(stringClass);else element.classList.add(stringClass);
+		};
+		init();
+	}();
+
+	
+	//Switch light/dark
+	
+	$("#switch").on('click', function () {
+		if ($("body").hasClass("light")) {
+			$("body").removeClass("light");
+			$("#switch").removeClass("switched");
+		}
+		else {
+			$("body").addClass("light");
+			$("#switch").addClass("switched");
+		}
+	});
+	
+})(jQuery); 
